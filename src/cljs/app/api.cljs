a676ee89d80dfc44f8d1de05a014b58aeffe80b6
NicolasMorenoAndrade <uselessconquest@gmail.com>
2025-07-31 19:26:36 -0500

    firebase user data flowing to db users table

--- src/cljs/app/api.cljs
+++ src/cljs/app/api.cljs
@@ -20,3 +20,23 @@
              json-data (.json response)
              data (js->clj json-data :keywordize-keys true)]
        data))))
+
+(defn verify-firebase-user
+  "Verifies a Firebase user with the backend API.
+
+   Makes a POST request to /api/user/verify with the user's Firebase data.
+   Returns a promise that resolves to the verified user data from the database.
+
+   Parameters:
+   - firebase-user: Map containing Firebase authentication data (:uid, :email, :display-name)"
+  [firebase-user]
+  (p/let [response (js/fetch "/api/user/verify"
+                             (clj->js {:headers {:Content-Type "application/json"}
+                                       :method "POST"
+                                       :body (js/JSON.stringify
+                                              #js {:firebase-uid (:uid firebase-user)
+                                                   :email (:email firebase-user)
+                                                   :display-name (:display-name firebase-user)})}))
+          json-data (.json response)
+          data (js->clj json-data :keywordize-keys true)]
+    data))
